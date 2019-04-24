5f37c90ce2bd3af02da2543ed72ad4b6058d2c56
Ardi Ragil Saputra <ardi.rs@gmail.com>
2019-04-24 14:33:44 +0700

    update dulu ini layout menu

---
+++ app/src/main/java/com/ardiery/gametk/title.java
@@ -0,0 +1,33 @@
+package com.ardiery.gametk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import 	android.os.Handler;
+import android.view.WindowManager;
+
+public class title extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        NextActivity();
+    }
+
+    public void NextActivity()
+    {
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent mainIntent;
+                mainIntent = new Intent(title.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
